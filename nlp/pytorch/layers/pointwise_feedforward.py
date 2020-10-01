32f9aa06502ecc08661295f10f5e2d9035da9229
knight <ian.dahlstrom@gmail.com>
2020-10-01 15:14:24 -0400

    migrating code into transformer.py, part 1

--- nlp/pytorch/layers/pointwise_feedforward.py
+++ nlp/pytorch/layers/pointwise_feedforward.py
@@ -5,19 +5,3 @@
 from torch import nn
 
 
-# -------------------------------------------------------------------------
-#   Create Layer
-# -------------------------------------------------------------------------
-class PointwiseFeedforward(nn.Sequential):
-    def __init__(
-            self, 
-            d_model:int, 
-            d_ff:int):
-        """
-        """
-        # Initialize Layer
-        super().__init__(
-                nn.Linear(d_model, d_ff),
-                nn.ReLU(),
-                nn.Linear(d_ff, d_model)
-        )
