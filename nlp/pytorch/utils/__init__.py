09adf7022c5792f85bee1faf8e22eacf0e91abc9
knight <ian.dahlstrom@gmail.com>
2020-10-06 14:23:59 -0400

    Added onnx code to __init__

--- nlp/pytorch/utils/__init__.py
+++ nlp/pytorch/utils/__init__.py
@@ -5,5 +5,11 @@ from .transformer import (
     create_padding_mask,
     create_look_ahead_mask,
     create_masks,
-    get_device
+    get_device,
+    create_transformer_dynamic_axes,
+    create_transformer_dummy_inputs
 )
+from .onnx import (
+        convert_model_to_onnx
+)
+import .constants
